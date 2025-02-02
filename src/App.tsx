0b809d3cae5bdb3b6fe87306b08b1300ec9207bb
killu21 <mgrzelak218@gmail.com>
2025-02-02 21:18:04 +0100

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [ color, setColor ] = useState('black'); 
+const App: React.FC = () => {
+  const [ color, setColor ] = useState<string>('black'); 
 
   const onclick = async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -19,17 +17,10 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Enter or select color</h1>
       <div className="card">
       <input 
+        style={{ marginBottom: '10px'}}
           type="text" 
           value={color} 
           onChange={(e) => setColor(e.target.value)} 
